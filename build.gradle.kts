9ad8622bb0023008159b474511cd074fa787f145
Matthias <56599453+12rcu@users.noreply.github.com>
2023-11-20 02:30:24 +0100

    update to minecraft 1.20.2
    also other dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
-    kotlin("plugin.serialization") version "1.9.0"
+    kotlin("jvm") version "1.9.20"
+    kotlin("plugin.serialization") version "1.9.20"
 }
 
 group = "cloud.luxor"
-version = "1.3.0"
+version = "1.3.1"
 
 repositories {
     mavenCentral()
@@ -19,9 +19,8 @@ dependencies {
     /**
      * paper
      */
-    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
-    implementation("com.charleskorn.kaml:kaml:0.54.0")
-    testImplementation(kotlin("test"))
+    compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
+    implementation("com.charleskorn.kaml:kaml:0.55.0")
 }
 
 java {
